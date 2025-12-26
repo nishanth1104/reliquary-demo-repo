5f3a2d888d6d592c355395962609ded735a00495
Slayer_1104 <69082260+nishanth1104@users.noreply.github.com>
2025-12-26 01:38:59 -0500

    Initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_root():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json()["message"] == "hello"
